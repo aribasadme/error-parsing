80bd6fff03c20ee5de205b82afe4edf95fb854d9
Albert Ribas <albert.ribas@flixbus.com>
2025-01-27 15:29:01 +0100

    Deploy SNS topic subscription in Terraform

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,4 +1,4 @@
-resource "aws_sns_topic" "error_parsing_alerts" {
+resource "aws_sns_topic" "alarm_topic" {
   name = var.sns_topic_name
 
   tags = {
@@ -7,4 +7,10 @@ resource "aws_sns_topic" "error_parsing_alerts" {
     Terraform              = "true"
     "Terraform Repository" = var.repository
   }
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.alarm_topic.arn
+  protocol  = "email"
+  endpoint  = var.email
 }
