f886409b2ff7934d485c09ac7cb835b374c54dca
Albert Ribas <albert.ribas@flixbus.com>
2025-01-28 09:24:29 +0100

    Fix clowdwatch log filter pattern

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_subscription_filter" "log_filter" {
   name            = "error-filter"
   log_group_name  = data.aws_cloudwatch_log_group.log_group.name
-  filter_pattern  = "[ERROR]"
+  filter_pattern  = "\"[ERROR]\""
   destination_arn = aws_lambda_function.error_parsing.arn
 }
